def173383bcf1b68a516d76752b69592fde39378
Eviatar Bach <eviatarbach@protonmail.com>
2018-03-25 18:44:25 -0700

    Adding example, naming function for simulations, text input of template

--- simulation_runner.py
+++ simulation_runner.py
@@ -6,8 +6,28 @@ import glob
 from mako.template import Template
 
 
-def run_simulation(command, config_path, template_path, single_parameters={},
-                   sweep_parameters={}, build=False, run=True):
+def hasher(keys, param_set):
+    return hash(''.join(map(str, param_set.values()))) % 65535
+
+def string(keys, param_set):
+    return '_'.join('{key}={param}'.format(key=key, param=param) for key, param in zip(keys, param_set))
+
+
+def run_simulation(command, config_path, template_path=None, template_text=None,
+                   single_parameters={}, sweep_parameters={}, naming=string,
+                   build=False, run=True, verbose=True):
+    '''
+    EXAMPLES:
+
+      >>> run_simulation('cat {sim_id}.txt', '{sim_id}.txt', template_text='Hello ${x*10}\n', sweep_parameters={'x': (1, 2, 3)}, verbose=False)
+      Hello 10
+      Hello 20
+      Hello 30
+    '''
+    if (((template_path is None) and (template_text is None))
+        or (not (template_path is None) and not (template_text is None))):
+        raise ValueError('Exactly one of `template_path` or `template_text` must be provided.')
+
     params = single_parameters.copy()
 
     if build:
@@ -22,7 +42,10 @@ def run_simulation(command, config_path, template_path, single_parameters={},
     else:
         product = [params]
 
-    config = Template(filename=template_path, input_encoding='utf-8')
+    if template_path:
+        config = Template(filename=template_path, input_encoding='utf-8')
+    else:
+        config = Template(text=template_text, input_encoding='utf-8')
 
     processes = []
     for param_set in product:
@@ -30,18 +53,18 @@ def run_simulation(command, config_path, template_path, single_parameters={},
         if keys:
             for index, value in enumerate(param_set):
                 sweep_params[keys[index]] = value
-        #TODO: use something better
-        sim_id = abs(sum(map(hash, sweep_params.values()))) % 65535
+
+        sim_id = naming(keys, sweep_params.values())
         sim_ids.append(sim_id)
 
         config_rendered = config.render(**sweep_params)
         config_file = open(config_path.format(sim_id=sim_id), 'w')
         config_file.write(config_rendered)
         config_file.close()
-
         if run:
-            print("Running simulation {sim_id} with parameters:".format(sim_id=sim_id))
-            print('\n'.join('{key}: {param}'.format(key=key, param=param) for key, param in zip(keys, param_set)))
+            if verbose:
+                print("Running simulation {sim_id} with parameters:".format(sim_id=sim_id))
+                print('\n'.join('{key}: {param}'.format(key=key, param=param) for key, param in zip(keys, param_set)))
             proc = subprocess.Popen(command.format(sim_id=sim_id), shell=True)
             processes.append(proc)
 
