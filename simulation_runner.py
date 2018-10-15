0cd70a3af3e022295e2834bf6aef6113b5533699
Eviatar Bach <eviatarbach@protonmail.com>
2018-10-14 22:05:44 -0400

    Documentation improvements

--- simulation_runner.py
+++ simulation_runner.py
@@ -1,5 +1,5 @@
 """
-Copyright Eviatar Bach (eviatarbach@protonmail.com) 2015–2018
+Copyright Eviatar Bach (eviatarbach@protonmail.com) 2015–2018.
 
 Licensed under the MIT License. See license text at
 https://opensource.org/licenses/MIT.
@@ -21,34 +21,59 @@ import xarray
 
 class _Namer(ABC):
     """
-    Abstract class for assigning simulation IDs to simulation. Only the `next`
-    method has to be implemented.
+    Abstract class for assigning simulation IDs to simulation.
+
+    Only the `next` method has to be implemented.
     """
+
     def start(self, length):
         pass
 
     @abstractmethod
     def next(self, keys, param_set):
-        """
-        Generate simulation ID for a given parameter set
-        """
+        """Generate simulation ID for a given parameter set."""
         pass
 
 
 class Sequential(_Namer):
     """
-    Name simulations with consecutive numbers and leading zeros
+    Name simulations with consecutive numbers and leading zeros.
+
+    EXAMPLES:
+    >>> counter = Sequential()
+    >>> counter.start(length=11)
+    >>> counter.next(['key1'], [['key_value1']])
+    '00'
+    >>> counter.next(['key2'], [['key_value2']])
+    '01'
+
+    >>> counter = Sequential(zfill=3, start_at=3)
+    >>> counter.start(length=2)
+    >>> counter.next(['key1'], [['key_value1']])
+    '003'
+    >>> counter.next(['key2'], [['key_value2']])
+    '004'
+    >>> counter.next(['key3'], [['key_value3']])
+    StopIteration
+
     """
+
     def __init__(self, zfill=None, start_at=0):
         self.zfill = zfill
         self.start_at = start_at
 
     def start(self, length):
+        """Initialize counter."""
         self.count = self.start_at - 1
         if self.zfill is None:
+            # Compute how many digits are needed to represent all the numbers
             self.zfill = math.floor(math.log10(length - 1) + 1)
+        self.length = length
 
     def next(self, keys, param_set):
+        """Return next simulation ID."""
+        if self.count + 1 >= self.length + self.start_at:
+            raise StopIteration
         self.count += 1
         return str(self.count).zfill(self.zfill)
 
@@ -58,14 +83,16 @@ def run_simulation(command, config_path, sweep_id=None, template_path=None,
                    sweep_parameters={}, naming=Sequential(), run=True,
                    verbose=True, delay=False):
     r"""
-    EXAMPLES:
+    Run parameter sweeps.
 
+    EXAMPLES:
     >>> run_simulation('cat {sim_id}.txt', '{sim_id}.txt',
     ...                template_text='Hello ${x*10}\n',
-    ...                sweep_parameters={'x': (1, 2, 3)}, verbose=False)
+    ...                sweep_parameters={'x': [1, 2, 3]}, verbose=False)
     Hello 10
     Hello 20
     Hello 30
+
     """
     if (((template_path is None) and (template_text is None))
             or (not (template_path is None) and not (template_text is None))):
